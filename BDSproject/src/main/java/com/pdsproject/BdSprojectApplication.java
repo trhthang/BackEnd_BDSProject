cbe9e53d1ec913b984fd511f0165e88b59278de8
Thang Tran <trhthang0401@gmail.com>
2021-12-01 21:15:44 +0700

    Back end Prj BĐS

---
+++ BDSproject/src/main/java/com/pdsproject/BdSprojectApplication.java
@@ -0,0 +1,13 @@
+package com.pdsproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BdSprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BdSprojectApplication.class, args);
+	}
+
+}
